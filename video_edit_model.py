5d5399e76befaebc46c7a99bdb0a9d6f418e4ce0
Zach Sickles <78000004+zsickles@users.noreply.github.com>
2024-05-11 22:11:12 -0400

    video editing

--- video_edit_model.py
+++ video_edit_model.py
@@ -5,24 +5,26 @@ import cv2
 
 class VideoEditModel:
     def __init__(self, video_path, features):
+        self.width = 640
+        self.height = 480
+
         self.video_path = video_path
         self.features = features
+
         cap = cv2.VideoCapture(video_path)
         self.fps = cap.get(cv2.CAP_PROP_FPS)
-        self.width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        
         cap.release()
         cv2.destroyAllWindows()
     
     def play(self, song_path):
        self.video_edits_disco()
         
-
     def video_edits_pop(self):
         pass
 
     def video_edits_disco(self):
-        cap = cv2.VideoCapture(0)
+        cap = cv2.VideoCapture(self.video_path)
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         out = cv2.VideoWriter('test.mp4', fourcc, self.fps, (640,480))
 
@@ -30,31 +32,35 @@ class VideoEditModel:
         current_time = 0.0
         current_beat = 0
 
-        purple_kernel  = np.full((self.width,self.height,3), (164,20,217), np.uint8)
-        orange_kernel  = np.full((self.width,self.height,3), (255, 128, 43), np.uint8)
-        yellow_kernel = np.full((self.width,self.height,3), (249, 225, 5), np.uint8)
-        green_kernel  = np.full((self.width,self.height,3), (52, 199, 165), np.uint8)
-        violet_kernel  = np.full((self.width,self.height,3), (93, 80, 206), np.uint8)
+        purple_kernel  = (164,20,217)
+        orange_kernel  = (255, 128, 43)
+        yellow_kernel = (249, 225, 5)
+        green_kernel  = (52, 199, 165)
+        violet_kernel  = (93, 80, 206)
 
 
         color_filters = [purple_kernel, orange_kernel, yellow_kernel, green_kernel, violet_kernel]
 
         while True:
-            print("here")
+
             ret, frame = cap.read()
-            if not ret:
+
+            if not ret or current_beat >= len(self.features) or current_beat > 5:
                 break
+
             if current_time > self.features[current_beat]:
                 current_beat += 1
 
             kernel_index = current_beat % 5
-            frame = cv2.addWeighted(frame, 0.8, color_filters[kernel_index], 0.2, 0)
 
-            print(frame)
+            frame = cv2.addWeighted(frame, 0.8, np.full((frame.shape[0],frame.shape[1],3), color_filters[kernel_index], np.uint8), 0.2, 0)
+            
+            frame_resized = cv2.resize(frame, (self.width, self.height))
 
-            out.write(frame)
+            out.write(frame_resized)
             frame_number += 1
             current_time = frame_number / self.fps
+            print(current_time)
 
         cap.release()
         out.release()
@@ -87,7 +93,7 @@ class VideoEditModel:
 
 
 if __name__ == "__main__":
-    # This code block will only execute if the file is executed directly, not imp
+    # This code block will only execute if the file is executed directly, not imported
     
     music_extractor = Extractor("songs/disco/dancing_queen.wav")
 
